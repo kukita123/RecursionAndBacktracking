1de4bea58594160dc97d3908fc7af1ca77482d0b
Mariana Hristozkova <kukita@abv.bg>
2021-08-20 13:19:32 +0300

    Recursion and backtracking solutions

--- Fibonacii/Program.cs
+++ Fibonacii/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fibonacii
 {
     class Program
     {
+        // Рекурсивен метод за намиране на фибоначи
+        static int fib(int n)
+        {
+            if (n == 1 || n == 2)
+                return 1;
+            else
+                return fib(n - 2) + fib(n - 1);
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("N=");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(fib(n));
         }
     }
 }
